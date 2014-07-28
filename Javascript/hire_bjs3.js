8709a320de51ff8c9d306b2f2b07cdf75d52dd81
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-07-27 21:39:36 -0400

    Started the app hire_bjs3.js to help employers decide whether or not to hire me. Wrote the first confirmation message to begin the program.

---
+++ Javascript/hire_bjs3.js
@@ -0,0 +1,6 @@
+//DOC: hire_bjs3.js
+//DESCRIPTION: A choose your own adventure game to determine whether you should hire Bobby Joe Smith III as a full-stack web developer
+
+//Check if user is ready to play!
+//If confirmation window pops up with message "Are you ready to play", then pass
+confirm("Are you ready for some clarity?");
