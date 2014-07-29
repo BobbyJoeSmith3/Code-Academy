3db3ca23227dacb2fab4d235db76ee2f29864773
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-07-29 13:04:15 -0400

    Added a function that compares user and computer choice in rock, paper, scissors

--- Javascript/rock_paper_scissors.js
+++ Javascript/rock_paper_scissors.js
@@ -12,3 +12,26 @@
 
 // Ask the user which option they want to pick (rock, paper or scissors) and save answer as a variable
 var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+//randomly generated computer choice
+var computerChoice = Math.random()
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} 
+
+console.log("Computer: " + computerChoice);
+
+//function to compare user choice and computer choice
+var compare = function (choice1, choice2) {
+    if (choice1 === choice2) {
+        return "The result is a tie!";
+    } else if (choice2 === "scissors") {
+        return "rock wins";
+    } else {
+        return "paper wins";
+    };    
+};
