e991a6ae58e4b9f0c6e3c3fa926ce45713360aeb
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-07-29 11:30:01 -0400

    Started a javascript app that allows the user to play 'Rock, Paper, Scissors' against the computer

---
+++ Javascript/rock_paper_scissors.js
@@ -0,0 +1,14 @@
+//DOC: rock_paper_scissors.js
+// The Game
+//Rock paper scissors is a classic 2 player game. Each player chooses either rock, paper or scissors. The possible outcomes:
+	// - Rock destroys scissors.
+	// - Scissors cut paper.
+	// - Paper covers rock.
+//Our code will break the game into 3 phases:
+	//a. User makes a choice
+	//b. Computer makes a choice
+	//c. A compare function will determine who wins
+
+
+// Ask the user which option they want to pick (rock, paper or scissors) and save answer as a variable
+var userChoice = prompt("Do you choose rock, paper or scissors?");
